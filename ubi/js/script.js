1fce2fef41ba356ceac79a66b8ff3cecd1533ffe
ashmyytaa <ashmi@live.ca>
2026-08-28 00:13:57 -0400

    changes

--- ubi/js/script.js
+++ ubi/js/script.js
@@ -6,24 +6,23 @@ function setup() {
 function draw() {
     background(15, 20, 45);
 
-    let pixelSize = 6;
+    let pixelSize = 10;
 
     for (let x = 0; x < width; x += pixelSize) {
         for (let y = 0; y < height; y += pixelSize) {
 
-            // Moving waves
+            ///moving waves
             let wave = sin(x * 0.02 + frameCount * 0.03);
             wave += sin(y * 0.03 - frameCount * 0.02);
             wave += sin((x + y) * 0.01 + frameCount * 0.04);
 
-            // Convert the waves into a rainbow
+            //turning the waves into rainbow colors
             let hue = (wave * 40 + frameCount * 0.5) % 360;
 
             colorMode(HSB, 360, 100, 100);
             fill(hue, 80, 100);
 
-            // Every square stays exactly the same size
-            // This makes the pixel effect much stronger
+            //pixel size
             rect(x, y, pixelSize, pixelSize);
         }
     }
