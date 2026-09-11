1e5aa160af81b11041aa3a649d79da74ff3de789
ashmyytaa <ashmi@live.ca>
2026-09-10 20:06:18 -0400

    updates

--- ubi/js/script.js
+++ ubi/js/script.js
@@ -6,17 +6,17 @@ function setup() {
 function draw() {
     background(15, 20, 45);
 
-    let pixelSize = 10;
+    let pixelSize = 11;
 
     for (let x = 0; x < width; x += pixelSize) {
         for (let y = 0; y < height; y += pixelSize) {
 
             ///moving waves
-            let wave = sin(x * 0.02 + frameCount * 0.03);
+            let wave = sin(x * 0.02 + frameCount * 0.04);
             wave += sin(y * 0.03 - frameCount * 0.02);
             wave += sin((x + y) * 0.01 + frameCount * 0.04);
 
-            //turning the waves into rainbow colors
+            //rainbow colors and its intensity
             let hue = (wave * 40 + frameCount * 0.5) % 360;
 
             colorMode(HSB, 360, 100, 100);
